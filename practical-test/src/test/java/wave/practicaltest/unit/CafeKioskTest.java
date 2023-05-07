7b9ea50d883e1aec86c49b16be9c8ad5f00d3847
waveofmymind <sjun0913@gmail.com>
2023-05-07 16:38:26 +0900

    [FEAT] 카페 키오스크 메서드별 단위 테스트 구현

--- practical-test/src/test/java/wave/practicaltest/unit/CafeKioskTest.java
+++ practical-test/src/test/java/wave/practicaltest/unit/CafeKioskTest.java
@@ -1,11 +1,24 @@
 package wave.practicaltest.unit;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import wave.practicaltest.unit.beverage.Americano;
+import wave.practicaltest.unit.beverage.Latte;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CafeKioskTest {
+
+    @Test
+    void add_manual_test() {
+        //given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
 
     @Test
     void add() {
@@ -13,13 +26,36 @@ class CafeKioskTest {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
-        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
 
-        //when
+    }
 
-        //then
+    @Test
+    void remove() {
+        //given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
 
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
 }
