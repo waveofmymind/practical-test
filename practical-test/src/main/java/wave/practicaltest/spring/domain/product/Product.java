3ce1a4d1f3a728f2d1e6e95ccf9b8eca82d3819c
waveofmymind <sjun0913@gmail.com>
2023-05-08 00:07:50 +0900

    [FEAT] JPA 테스트를 위한 도메인 레이어 구현

---
+++ practical-test/src/main/java/wave/practicaltest/spring/domain/product/Product.java
@@ -0,0 +1,31 @@
+package wave.practicaltest.spring.domain.product;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingStatus sellingStatus;
+
+    private String name;
+
+    private int price;
+
+
+}
---
+++ practical-test/src/main/java/wave/practicaltest/spring/domain/product/ProductSellingStatus.java
@@ -0,0 +1,21 @@
+package wave.practicaltest.spring.domain.product;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public enum ProductSellingStatus {
+
+    SELLING("판매중"),
+    HOLD("판매보류"),
+    STOP_SELLING("판매중지");
+
+    private final String text;
+
+    public static List<ProductSellingStatus> forDisplay() {
+        return List.of(SELLING, HOLD);
+    }
+}
