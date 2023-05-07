643ce13bc0e02ccdcd6ffbe841c46374e94deed1
waveofmymind <sjun0913@gmail.com>
2023-05-07 20:13:21 +0900

    [FEAT] 테스트하기 어려운 영역 분리

--- practical-test/src/main/java/wave/practicaltest/unit/CafeKiosk.java
+++ practical-test/src/main/java/wave/practicaltest/unit/CafeKiosk.java
@@ -5,11 +5,16 @@ import wave.practicaltest.unit.beverage.Beverage;
 import wave.practicaltest.unit.order.Order;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CafeKiosk {
+
+    public static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+    public static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(22, 0);
+
 
     private final List<Beverage> beverages = new ArrayList<>();
 
@@ -44,6 +49,24 @@ public class CafeKiosk {
     }
 
     public Order createOrder() {
-        return new Order(LocalDateTime.now(),beverages);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(currentDateTime, beverages);
+    }
+
+    public Order createOrder(LocalDateTime currentDateTime) {
+        LocalTime currentTime = currentDateTime.toLocalTime();
+
+
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(currentDateTime, beverages);
     }
 }
--- practical-test/src/test/java/wave/practicaltest/unit/CafeKioskTest.java
+++ practical-test/src/test/java/wave/practicaltest/unit/CafeKioskTest.java
@@ -4,6 +4,9 @@ package wave.practicaltest.unit;
 import org.junit.jupiter.api.Test;
 import wave.practicaltest.unit.beverage.Americano;
 import wave.practicaltest.unit.beverage.Latte;
+import wave.practicaltest.unit.order.Order;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -80,6 +83,30 @@ class CafeKioskTest {
 
         cafeKiosk.clear();
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void createOrder() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        Order order = cafeKiosk.createOrder();
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void createOrderOutsideOpenTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2023,1,17,9,59)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
     }
 
 }
