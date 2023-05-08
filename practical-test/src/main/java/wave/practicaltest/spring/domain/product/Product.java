8e16dac234e93a8ec479df1b49fa99d9a915d1e5
waveofmymind <sjun0913@gmail.com>
2023-05-08 15:11:36 +0900

    [FEAT] 엔티티클래스 빌더 추가

--- practical-test/src/main/java/wave/practicaltest/spring/domain/product/Product.java
+++ practical-test/src/main/java/wave/practicaltest/spring/domain/product/Product.java
@@ -1,6 +1,7 @@
 package wave.practicaltest.spring.domain.product;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,5 +28,12 @@ public class Product {
 
     private int price;
 
-
+    @Builder
+    public Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
