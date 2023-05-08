78e206e41b020a8f543652d845b9066d17f8861e
waveofmymind <sjun0913@gmail.com>
2023-05-08 17:59:27 +0900

    [FEAT] CQRS 트랜잭션 어노테이션 처리

--- practical-test/src/main/java/wave/practicaltest/spring/api/service/product/ProductService.java
+++ practical-test/src/main/java/wave/practicaltest/spring/api/service/product/ProductService.java
@@ -8,27 +8,32 @@ import wave.practicaltest.spring.api.service.product.response.ProductResponse;
 import wave.practicaltest.spring.domain.product.Product;
 import wave.practicaltest.spring.domain.product.ProductRepository;
 import wave.practicaltest.spring.domain.product.ProductSellingStatus;
-import wave.practicaltest.spring.domain.product.ProductType;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Query
+ *
+ */
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    // 동시성 이슈
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         String nextProductNumber = createNextProductNumber();
-        return ProductResponse.builder()
-                .productNumber(nextProductNumber)
-                .type(request.getType())
-                .sellingStatus(request.getSellingStatus())
-                .name(request.getName())
-                .price(request.getPrice())
-                .build();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+        return ProductResponse.of(savedProduct);
     }
 
     private String createNextProductNumber() {
