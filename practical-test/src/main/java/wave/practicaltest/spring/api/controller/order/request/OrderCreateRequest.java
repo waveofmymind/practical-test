17acff11a5d557aba0316ab0f374f4fd42506e15
waveofmymind <sjun0913@gmail.com>
2023-05-08 15:32:16 +0900

    [FIX] 오류 수정 및 반환값 변경

--- practical-test/src/main/java/wave/practicaltest/spring/api/controller/order/request/OrderCreateRequest.java
+++ practical-test/src/main/java/wave/practicaltest/spring/api/controller/order/request/OrderCreateRequest.java
@@ -1,11 +1,14 @@
 package wave.practicaltest.spring.api.controller.order.request;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderCreateRequest {
 
     private List<String> productNumbers;
