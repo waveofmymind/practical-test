e24e370b0764835b5f1baa33a6f6a72890856598
waveofmymind <sjun0913@gmail.com>
2023-05-08 20:43:18 +0900

    [FEAT] 공통 응답 DTO 및 예외 처리 구현

---
+++ practical-test/src/main/java/wave/practicaltest/spring/api/ApiResponse.java
@@ -0,0 +1,37 @@
+package wave.practicaltest.spring.api;
+
+import lombok.Getter;
+import org.apache.tomcat.util.http.parser.HttpParser;
+import org.springframework.http.HttpStatus;
+import wave.practicaltest.spring.api.service.product.response.ProductResponse;
+
+@Getter
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+
+    private String message;
+
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T>  ApiResponse<T> of(HttpStatus httpStatus,String message, T data) {
+        return new ApiResponse<>(httpStatus, message, data);
+    }
+
+    public static <T>  ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return of(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T>  ApiResponse<T> ok(T data) {
+        return of(HttpStatus.OK,data);
+    }
+
+}
