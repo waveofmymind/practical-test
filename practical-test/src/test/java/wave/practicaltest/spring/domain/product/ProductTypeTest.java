b29524caff37a932da0ef7286ed4e7c0721e6d5a
waveofmymind <sjun0913@gmail.com>
2023-05-13 12:34:22 +0900

    @ParameterizedTest 추가

--- practical-test/src/test/java/wave/practicaltest/spring/domain/product/ProductTypeTest.java
+++ practical-test/src/test/java/wave/practicaltest/spring/domain/product/ProductTypeTest.java
@@ -3,6 +3,8 @@ package wave.practicaltest.spring.domain.product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +34,40 @@ class ProductTypeTest {
         Assertions.assertThat(result).isTrue();
 
     }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType3() {
+        //given
+        ProductType givenType1= ProductType.BAKERY;
+        ProductType givenType2 = ProductType.BOTTLE;
+        ProductType givenType3 = ProductType.HANDMADE;
+        //when
+        boolean result1 = ProductType.containsStockType(givenType1);
+        boolean result2 = ProductType.containsStockType(givenType2);
+        boolean result3 = ProductType.containsStockType(givenType3);
+        //then
+
+        Assertions.assertThat(result1).isTrue();
+        Assertions.assertThat(result2).isTrue();
+        Assertions.assertThat(result3).isFalse();
+
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource({"BAKERY, true", "BOTTLE, true", "HANDMADE, false"})
+    @ParameterizedTest
+    void containsStockType4(ProductType productType,boolean expected) {
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        //then
+
+        Assertions.assertThat(result).isEqualTo(expected);
+
+    }
+
+
 
 
 }
