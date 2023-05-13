f2bc4a720b8822b3d938e9a0c9a9c404ef00ab1a
waveofmymind <sjun0913@gmail.com>
2023-05-13 13:56:55 +0900

    #1 [FEAT] dynamicTest 학습

--- practical-test/src/test/java/wave/practicaltest/spring/domain/stock/StockTest.java
+++ practical-test/src/test/java/wave/practicaltest/spring/domain/stock/StockTest.java
@@ -2,17 +2,24 @@ package wave.practicaltest.spring.domain.stock;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StockTest {
 
+    public static final Stock stock = Stock.create("001", 1);
+
     @DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다.")
     @Test
     void isQuantityLessThan() {
         //given
-        Stock stock = Stock.create("001", 1);
         int quantity = 2;
         //when
         boolean result = stock.isQuantityLessThan(quantity);
@@ -25,7 +32,6 @@ class StockTest {
     @Test
     void deductQuantity() {
         //given
-        Stock stock = Stock.create("001", 1);
         int quantity = 1;
         //when
         stock.deductQuantity(quantity);
@@ -37,11 +43,51 @@ class StockTest {
     @Test
     void deductQuantity2() {
         //given
-        Stock stock = Stock.create("001", 1);
         int quantity = 2;
         //when & then
         Assertions.assertThatThrownBy(() -> stock.deductQuantity(quantity))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("차감할 재고 수량이 없습니다.");
+    }
+
+    @DisplayName("")
+    @TestFactory
+    Collection<DynamicTest> dynamicTest() {
+        return List.of(
+                DynamicTest.dynamicTest("", () -> {
+
+                }),
+
+                DynamicTest.dynamicTest("", () -> {
+
+                })
+        );
+
+    }
+
+    @DisplayName("재고 차감 시나리오")
+    @TestFactory
+    Collection<DynamicTest> stockDeductionDynamicTest() {
+        //given
+        Stock stock = Stock.create("001", 1);
+
+        return List.of(
+                DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+                    //given
+                    int quantity = 1;
+                    //when
+                    stock.deductQuantity(quantity);
+                    //then
+                    Assertions.assertThat(stock.getQuantity()).isZero();
+                }),
+                DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+                    //given
+                    int quantity = 2;
+                    //when & then
+                    Assertions.assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage("차감할 재고 수량이 없습니다.");
+                })
+        );
     }
 }
