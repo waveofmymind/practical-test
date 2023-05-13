8da556359abd9b37bea4fbc01b1034081e3e2261
waveofmymind <sjun0913@gmail.com>
2023-05-13 12:37:39 +0900

    메소드 소스를 활용한 ParameterizedTest

--- practical-test/src/test/java/wave/practicaltest/spring/domain/product/ProductTypeTest.java
+++ practical-test/src/test/java/wave/practicaltest/spring/domain/product/ProductTypeTest.java
@@ -4,7 +4,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -58,6 +62,27 @@ class ProductTypeTest {
     @CsvSource({"BAKERY, true", "BOTTLE, true", "HANDMADE, false"})
     @ParameterizedTest
     void containsStockType4(ProductType productType,boolean expected) {
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        //then
+
+        Assertions.assertThat(result).isEqualTo(expected);
+
+    }
+
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.BAKERY, true),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.HANDMADE, false)
+        );
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    void containsStockType5(ProductType productType,boolean expected) {
         //when
         boolean result = ProductType.containsStockType(productType);
 
